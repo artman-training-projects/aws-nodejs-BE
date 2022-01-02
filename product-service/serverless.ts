f2ce7a77ab2ba15b63c582e89026f789d06b1868
ArtMan-8 <ya.Artriches@ya.ru>
2022-01-02 13:37:35 +0300

    add: env

--- product-service/serverless.ts
+++ product-service/serverless.ts
@@ -3,9 +3,14 @@ import type { AWS } from "@serverless/typescript";
 import getProductsList from "@functions/getProductsList";
 import getProductsById from "@functions/getProductsById";
 
+import { mainEnv, databaseEnv } from "../.env";
+
+const environment = { ...mainEnv, ...databaseEnv };
+
 const serverlessConfiguration: AWS = {
 	service: "product-service",
 	frameworkVersion: "2",
+	useDotenv: true,
 	plugins: ["serverless-esbuild"],
 	provider: {
 		name: "aws",
@@ -15,10 +20,7 @@ const serverlessConfiguration: AWS = {
 			minimumCompressionSize: 1024,
 			shouldStartNameWithService: true,
 		},
-		environment: {
-			AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
-			NODE_OPTIONS: "--enable-source-maps --stack-trace-limit=1000",
-		},
+		environment,
 		lambdaHashingVersion: "20201221",
 	},
 	// import the function via paths
@@ -29,7 +31,7 @@ const serverlessConfiguration: AWS = {
 			bundle: true,
 			minify: false,
 			sourcemap: true,
-			exclude: ["aws-sdk"],
+			exclude: ["aws-sdk", "pg-native"],
 			target: "node14",
 			define: { "require.resolve": undefined },
 			platform: "node",
