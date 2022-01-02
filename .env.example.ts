4f12e43b70e00d1cac05b6f957723affc5c9f6ee
ArtMan-8 <ya.Artriches@ya.ru>
2022-01-02 13:37:35 +0300

    add: env

---
+++ .env.example.ts
@@ -0,0 +1,11 @@
+export const databaseEnv = {
+	PG_NAME: "db_name",
+	PG_HOST: "db_host",
+	PG_PORT: "db_port",
+	PG_USER: "db_user",
+	PG_PASS: "db_pass",
+};
+
+export const mainEnv = {
+	SOME_OPTION: "value",
+};
