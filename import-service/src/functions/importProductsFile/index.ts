e6610efdfb299aa2c881d48e0b98ca80a605d7f5
ArtMan-8 <ya.Artriches@ya.ru>
2022-02-06 23:13:14 +0300

    feat: Authorization service

--- import-service/src/functions/importProductsFile/index.ts
+++ import-service/src/functions/importProductsFile/index.ts
@@ -1,4 +1,5 @@
 import { handlerPath } from "@libs/handlerResolver";
+import { aws } from "../../../../.env";
 
 export default {
 	handler: `${handlerPath(__dirname)}/handler.main`,
@@ -7,6 +8,14 @@ export default {
 			http: {
 				method: "get",
 				path: "import",
+				cors: true,
+				authorizer: {
+					name: "AuthImportService",
+					arn: `arn:aws:lambda:${aws.REGION}:${aws.ACCOUNT}:function:authorization-service-dev-basicAuthorizer`,
+					resultTtlInSeconds: 0,
+					identitySource: "method.request.header.Authorization",
+					type: "token",
+				},
 			},
 		},
 	],
